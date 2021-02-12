0b09407b6e6de94c9661dc451ac3b95d867ce687
jeffubayi <jeffubayi@gmail.com>
2021-02-12 08:24:46 +0300

    styles - add global styles

---
+++ src/Globalstyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+`;
+
+export const Header = styled.h2`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white; 
+  text-align: center;
+`;
